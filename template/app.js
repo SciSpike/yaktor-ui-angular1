9d4ec1e1f06d30a7359c94d2d7d91d230ad0f293
Amit Kapadia <akapad@gmail.com>
2013-12-23 10:17:06 -0600

    pass data on state change

--- template/app.js
+++ template/app.js
@@ -10,7 +10,14 @@
         {{#states}}
         .state('{{name}}', {
           url: '/{{url}}',
-          templateUrl: 'partial/{{url}}.html'
+          templateUrl: 'partial/{{url}}.html',
+          controller: function($scope) {
+            
+            $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+              console.log($scope.data);
+            });
+            
+          }
         })
         {{/states}}
       
