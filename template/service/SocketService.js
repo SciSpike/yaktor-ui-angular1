b65f7ef2d5a11630f98ed995e6872535d0f6596e
Amit Kapadia <akapad@gmail.com>
2013-12-30 08:41:36 -0800

    template for socket service

---
+++ template/service/SocketService.js
@@ -0,0 +1,8 @@
+
+'use strict';
+
+angular.module('{{appname}}').service('SocketService', function ($rootScope) {
+  
+  // TODO: Create all socket connections here based on Jonathan's specs
+  console.log("HERE WE ARE IN A SERVICE");
+});
