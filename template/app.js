90220fd29e72bc661919275491ed97b2041e32fc
Amit Kapadia <akapad@gmail.com>
2014-01-06 14:08:56 -0800

    modularize controllers

--- template/app.js
+++ template/app.js
@@ -11,15 +11,7 @@
         .state('{{name}}', {
           url: '/{{url}}',
           templateUrl: 'partial/{{url}}.html',
-          controller: function($scope, SocketService) {
-            
-            $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
-              
-              // TODO: Use the SocketService to push events with data to the server
-              console.log($scope.data);
-            });
-            
-          }
+          controller: '{{controller}}Ctrl'
         })
         {{/states}}
       
---
+++ template/controller/controller.js
@@ -0,0 +1,10 @@
+
+angular.module('SodaPurchaser')
+  .controller('{{name}}Ctrl', ['$scope', 'SocketService', function ($scope, SocketService) {
+    
+    // TODO: Use the SocketService to push events with data to the server
+    $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+      console.log($scope.data);
+    });
+  
+}]);
