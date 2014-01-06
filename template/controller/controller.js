f75c4181bd8a25fca9f3efd60ce728e5448b7378
Amit Kapadia <akapad@gmail.com>
2014-01-06 14:23:55 -0800

    remove old code

--- template/controller/controller.js
+++ template/controller/controller.js
@@ -1,6 +1,9 @@
 
 angular.module('SodaPurchaser')
   .controller('{{name}}Ctrl', ['$scope', 'SocketService', function ($scope, SocketService) {
+    $scope.data = {};
+    
+    
     
     // TODO: Use the SocketService to push events with data to the server
     $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
