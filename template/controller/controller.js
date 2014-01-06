d4bafb77c8961bde8dec92cfdb56732d8119fb93
Amit Kapadia <akapad@gmail.com>
2014-01-06 15:08:25 -0800

    initializing scope variables in controllers

--- template/controller/controller.js
+++ template/controller/controller.js
@@ -3,7 +3,13 @@ angular.module('SodaPurchaser')
   .controller('{{name}}Ctrl', ['$scope', 'SocketService', function ($scope, SocketService) {
     $scope.data = {};
     
+    {{#scopeVariables}}
+    $scope.data.{{stateName}} = {};
+    {{#variables}}
+    $scope.data.{{stateName}}.{{variable}} = {{type}};
+    {{/variables}}
     
+    {{/scopeVariables}}
     
     // TODO: Use the SocketService to push events with data to the server
     $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
