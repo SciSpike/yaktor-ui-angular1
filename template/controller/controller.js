d3a7c147ba7cc745865ecbf1787deb4ff341a793
Amit Kapadia <akapad@gmail.com>
2014-01-07 12:43:13 -0800

    broadcast only data relevant to transitioned state

--- template/controller/controller.js
+++ template/controller/controller.js
@@ -13,7 +13,8 @@ angular.module('SodaPurchaser')
     
     // TODO: Use the SocketService to push events with data to the server
     $scope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
-      console.log($scope.data);
+      var stateName = toState.name;
+      console.log($scope.data[stateName]);
     });
   
 }]);
