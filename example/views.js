d61e90262eb8ddf6477f6da7a34542edca8c0197
JK <jonathan.kamke@gmail.com>
2014-01-20 10:33:14 -0800

    re-add rawmeta_test.js;seperate base portion of meta schema; add schema for views;

---
+++ example/views.js
@@ -0,0 +1,64 @@
+module.exports={
+  "/page.html":{
+      "POST":{"type":{
+        "properties" : {
+          "id" : {
+            "type": "integer"
+          },
+          "subTypes" : {
+            "type": "array",
+            "items" : {
+              "$typeRef":"Domain.Entity",
+              "type": "integer"
+            }
+          }
+        }
+      }
+      },
+      "GET":{"type":{
+        "properties" : {
+          "id" : {
+            "type": "integer"
+          },
+          "subTypes" : {
+            "type": "array",
+            "items" : {
+              "$typeRef":"Domain.Entity",
+              "type": "integer"
+            }
+          }
+        }
+      }
+      },
+      "PUT":{"type":{
+        "properties" : {
+          "id" : {
+            "type": "integer"
+          },
+          "subTypes" : {
+            "type": "array",
+            "items" : {
+              "$typeRef":"Domain.Entity",
+              "type": "integer"
+            }
+          }
+        }
+      }
+      },
+      "FIND":{"type":{
+        "properties" : {
+          "id" : {
+            "type": "integer"
+          },
+          "subTypes" : {
+            "type": "array",
+            "items" : {
+              "$typeRef":"Domain.Entity",
+              "type": "integer"
+            }
+          }
+        }
+      }
+      }
+  }
+}
