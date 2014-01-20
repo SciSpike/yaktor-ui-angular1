93d28cb852a316b9822f94a18b1f34e876fa2de7
Amit Kapadia <akapad@gmail.com>
2014-01-20 11:31:57 -0800

    make sure generated ui model validates

--- template/app.js
+++ template/app.js
@@ -17,24 +17,24 @@
       
     });
   
-  angular.module('{{appname}}')
-    .controller("ConnectionCtrl", function($scope, $http, $cookies) {
-      $scope.isConnected = true;
-      var host = "http://0.0.0.0:3000";
-      
-      $http.get(host)
-        .success(function() {
-          var sessionId = $cookies["connect.sid"].replace(/s:([^\.]*).*/, "$1");
-          window.SodaPurchase.purchaser.socket.connectWithPrefix(host, sessionId, true, function() {
-            $scope.isConnected = true;
-            
-            
-          });
-        })
-        .error(function(data, status, headers, config) {
-          $scope.isConnected = false;
-        });
-      
-    });
+  // angular.module('{{appname}}')
+  //   .controller("ConnectionCtrl", function($scope, $http, $cookies) {
+  //     $scope.isConnected = true;
+  //     var host = "http://0.0.0.0:3000";
+  //     
+  //     $http.get(host)
+  //       .success(function() {
+  //         var sessionId = $cookies["connect.sid"].replace(/s:([^\.]*).*/, "$1");
+  //         window.SodaPurchase.purchaser.socket.connectWithPrefix(host, sessionId, true, function() {
+  //           $scope.isConnected = true;
+  //           
+  //           
+  //         });
+  //       })
+  //       .error(function(data, status, headers, config) {
+  //         $scope.isConnected = false;
+  //       });
+  //     
+  //   });
     
 })();
--- template/service/SocketService.js
+++ template/service/SocketService.js
@@ -3,16 +3,16 @@
 
 angular.module('{{appname}}').service('SocketService', function ($rootScope, socketServer) {
   
-  // TODO: Create all socket connections here based on Jonathan's specs
-  
+  // // TODO: Create all socket connections here based on Jonathan's specs
+  // 
   var service = {};
-  
-  service.socket = new SockJS(socketServer);
-  service.emitter = new Emitter();
-  
-  service.onload = function(e) {
-    console.log('dummy onload function');
-  }
+  // 
+  // service.socket = new SockJS(socketServer);
+  // service.emitter = new Emitter();
+  // 
+  // service.onload = function(e) {
+  //   console.log('dummy onload function');
+  // }
   
   return service;
 });
