695c970570636806a6986c69956423811885344a
Peter Unsworth <peterjunsworth@gmail.com>
2014-03-10 13:13:33 -0500

    grunt

--- template/Gruntfile.js
+++ template/Gruntfile.js
@@ -4,31 +4,53 @@ module.exports = function(grunt) {
 	
   var config, dir;
   
+  var basePath = grunt.option('basePath') || "./";
+  
+  console.log(basePath);
+  
  dir = require("./gruntTasks/dirs");
   
   try {
-    dir.app = require("<%= basePath %>/bower.json").appPath || dir.app;
+    dir.app = require("./bower.json").appPath || dir.app;
   } catch (_error) {}
   
   config = {
-	basePath: "./",
+	basePath: basePath,
     dir: dir,
     browserify: {
-    	  dist: {
-    	    files: {
-    	  		'<%= basePath %>module.js': ['<%= basePath %>*.js']
-    	    },
-    	    options: {
-    	      
-    	    }
-    	  }
-      }
+	  dist: {
+	  	files: {
+	  		'./libs/lib.js': ['./controller/*.js']
+		},
+		options: {
+			//transform: ['uglifyify']
+		}
+	  }
+	}/*,
+	uglify: {
+		build: {
+			files: [{
+				expand: true,
+                src: '*.js',
+                dest: 'build/scripts',
+                cwd: './libs'
+            }]
+          }
+	}
+    */
   };
   
   grunt.initConfig(config);
   
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('uglifyjs');
   
   grunt.registerTask('default', ['browserify:dist']);
+  //grunt.registerTask('default', ['uglify:build']);
+  
+  /*grunt.registerTask('default', 'Set a config property.', function(name, val) {
+	  grunt.config.set(name, val);
+	});*/
+
   
 };
