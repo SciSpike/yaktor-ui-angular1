6c7a184a7cd542c09ea4adb870a21561800e5ce1
JK <jonathan.kamke@gmail.com>
2014-03-26 10:19:11 -0700

    fix enum;browserify more stuff

--- template/gruntTasks/browserify.js
+++ template/gruntTasks/browserify.js
@@ -28,13 +28,21 @@ module.exports = {
 	  },
 	  appDep: {
 		  	files: {
+          './libs/app.js': ["./app.js","constant/*.js","service/*.js"],
           './libs/controllers.js': ['./controller/*.js'],
           './libs/locale.js': ['./locale/*.js'],
 		  		'./libs/directives.js': ['./modules/**/*.js'],
-		  		'./libs/custom_controllers.js': ['./controller/custom/*.js']
+		  		'./libs/custom_controllers.js': ['./controller/custom/*.js'],
+	        './libs/resources.js': ["bower_components/jquery/dist/jquery.min.js"
+                                   ,"bower_components/angular/angular.js"
+                                   ,"bower_components/angular-translate/angular-translate.js"
+                                   ,"bower_components/angular-local-storage/angular-local-storage.js"
+                                   ,"bower_components/angular-ui-router/release/angular-ui-router.js"
+                                   ,"bower_components/angular-bootstrap/ui-bootstrap-tpls.js"
+                                   ,"bower_components/sockjs-client/sockjs.min.js"]
 			},
 			options: {
-				
+			  external:['socketApi']
 			}
 	  }
 };
