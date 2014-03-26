0f4b26988177bc3d92facd084e6cd014ef4a94f0
Peter Unsworth <peterjunsworth@gmail.com>
2014-03-26 16:55:40 -0500

    $

--- template/gruntTasks/browserify.js
+++ template/gruntTasks/browserify.js
@@ -1,10 +1,11 @@
 var browserifyLibExternal = [
-	
+  
 ];
 
 var browserifyLibAlias = [
   './bower_components/emitter/index.js:emitter-component',
   './bower_components/node-querystring/index.js:qs',
+  "./bower_components/jquery/dist/jquery.min.js:$",
   '../node_modules/tv4/tv4.js:tv4',
   '../public/socketApi.js:socketApi'
 ];
@@ -33,8 +34,7 @@ module.exports = {
           './libs/locale.js': ['./locale/*.js'],
 		  		'./libs/directives.js': ['./modules/**/*.js'],
 		  		'./libs/custom_controllers.js': ['./controller/custom/*.js'],
-	        './libs/resources.js': ["bower_components/jquery/dist/jquery.min.js"
-                                   ,"bower_components/angular/angular.js"
+	        './libs/resources.js': ["bower_components/angular/angular.js"
                                    ,"bower_components/angular-translate/angular-translate.js"
                                    ,"bower_components/angular-local-storage/angular-local-storage.js"
                                    ,"bower_components/angular-ui-router/release/angular-ui-router.js"
