91fa6677dc7592a1549c036828792bd36fddb53c
Peter Unsworth <peterunsworth@Peters-MacBook-Air.local>
2014-11-25 11:36:28 -0600

    removed blank objects and empty strings

--- template/modules/crudController.js
+++ template/modules/crudController.js
@@ -243,31 +243,42 @@ angular.module('<%- parentStateName %>')
 				  		});                 
 				  <%}%>
 				  var answers = {};
-	        function returnAnswers(dataObject, answersObject){
+	        function returnAnswers(dataObject, answersObject, prevObject, prevKey){
 	          switch(dataObject.constructor.name.toLowerCase()) {
               case 'array':
                 for(var i=0; i<dataObject.length; i++){
                   answersObject[i] = {};
-                  returnAnswers(dataObject[i], answersObject[i]);
+                  returnAnswers(dataObject[i], answersObject[i], answersObject, key);
                 }
                 break;
             }
 	          for(key in dataObject){
 	            if(dataObject[key]){
-	              if(dataObject[key].answer || dataObject[key].answer == ''){
+	              if(dataObject[key].answer){
 	                if(dataObject[key].typeRef){
 	                  dataObject[key].answer = dataObject[key].answer._id;
 	                }
-                  answersObject[key] = dataObject[key].answer;
+	                if(dataObject[key].answer != ''){
+	                  answersObject[key] = dataObject[key].answer;
+	                }else{
+	                  delete answersObject[key]; 
+	                }
                 }else{
                   switch(dataObject[key].constructor.name.toLowerCase()) {
                     case 'array':
                       answersObject[key] = [];
-                      returnAnswers(dataObject[key], answersObject[key]);
+                      returnAnswers(dataObject[key], answersObject[key], answersObject, key);
                       break;
                     case 'object':
-                      answersObject[key] = {};
-                      returnAnswers(dataObject[key], answersObject[key]);
+                      if(key != 'ui'){
+                        answersObject[key] = {};
+                        returnAnswers(dataObject[key], answersObject[key], answersObject, key);
+                      }
+                      break;
+                    default:
+                      if(prevObject && prevObject[prevKey]){
+                        delete prevObject[prevKey];
+                      }
                       break;
                   }
                 }
@@ -275,8 +286,21 @@ angular.module('<%- parentStateName %>')
 	          }
 	          return answers;
 	        }
+	        function cleanData(answerObject){
+	           for(key in answerObject){
+	               if(answerObject[key].constructor.name.toLowerCase() == 'object'){
+	                 if($.isEmptyObject(answerObject[key])){
+	                   delete answerObject[key]; 
+	                 }else{
+	                   cleanData(answerObject[key]);
+	                 }
+	               }
+	           }
+	           return answerObject
+	        }
 				  $scope.submitForm = function(type){
 					  var data = returnAnswers($scope.directiveData, answers);
+					  data = cleanData(data);
 					  <%- parentStateName %>Services.<%- state.ui.title.toLowerCase()%><%- parentStateName%>(data, id).then(function(response) {
 			           	  	$scope.changeState('main.<%- parentStateName %>.FIND', {id: 1});
 			            });
