75cfcddec35130ad4ab86eca93ab473dedc9778c
Gary Acord <gary@acordinfo.com>
2015-05-07 09:42:22 -0500

    agentServices, clean out some stupidity

--- template/modules/agentServices.js
+++ template/modules/agentServices.js
@@ -13,33 +13,16 @@ angular.module('<%- moduleName %>')
                 $rootScope.$emit($eventsCommon.conversations.<%- actions.url.replace('/', '')%>, emitData);
               });
           } else {
-              SocketService.doAction('<%- actions.url%>','init', initData, initData, function(err,data){
-                //WHAT HAPPENS HERE AND DOES THIS EVER GET CALLED?????
-              });
+              SocketService.doAction('<%- actions.url%>','init', initData, initData, function(err,data){});
           }
     }
-    
     <%
     for(state in states){
       for(element in states[state].elements){
         var elementName = element.toLowerCase()%>
     var _on_<%- elementName%> = function(initData, data){
       var data = data;
-      if(SocketService['<%- element%>']){
-        SocketService['<%- element%>']('<%- states[state].url%>',data, data, function(err,stateName){
-           var nextState = stateName.replace('state:', '');
-           var emitData = {
-                    data: data,
-                    nextState: nextState
-                }
-           $rootScope.$emit($eventsCommon.conversations.<%- actions.url.replace('/', '')%>, emitData);
-               //$rootScope.$emit($eventsCommon.conversations.<%- states[state].url.replace('/', '')%>.<%- element%>, emitData);
-        });
-      } else {
-        SocketService.doAction('<%- actions.url%>', '<%- element%>', initData, data, function(err,data){
-          //WHAT HAPPENS HERE
-        });
-      }
+        SocketService.doAction('<%- actions.url%>', '<%- element%>', initData, data, function(err,data){});
     }
       <%}
     }%>
